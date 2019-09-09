1db3248afe1c66f07bd897bd631fe32af6cbc141
Austin Hedeen <austin.hedeen@gmail.com>
2019-09-08 17:51:50 -0700

    Document data model classes

--- src/models/memory.js
+++ src/models/memory.js
@@ -2,17 +2,35 @@
 
 const uuid = require('uuid/v4');
 
+/** Class representing a generic in-memory model.
+ *
+ *  Acknowledgements: Thanks to Vinicio Sanchez and Code Fellows for providing
+ *  this interface.
+ */
 class Model {
 
+  /**
+   * Model Constructor - initializes data array
+   */
   constructor() {
     this.database = [];
   }
 
+  /**
+   * Retrieves one or more records
+   * @param id {string} optional record id
+   * @returns {*}
+   */
   get(id) {
     let response = id ? this.database.filter((record) => record.id === id) : this.database;
     return Promise.resolve(response);
   }
 
+  /**
+   * Create a new record
+   * @param entry {object} matches the format of the schema
+   * @returns {*}
+   */
   create(entry) {
     entry.id = uuid();
     let record = this.sanitize(entry);
@@ -20,17 +38,33 @@ class Model {
     return Promise.resolve(record);
   }
 
+  /**
+   * Replaces a record in the database
+   * @param id {string} Record ID
+   * @param entry {object} The record data to replace. ID is a required field
+   * @returns {*}
+   */
   update(id, entry) {
     let record = this.sanitize(entry);
     if (record.id) { this.database = this.database.map((item) => (item.id === id) ? record : item); }
     return Promise.resolve(record);
   }
 
+  /**
+   * Deletes a record in the model
+   * @param id {string} Record ID
+   * @returns {*}
+   */
   delete(id) {
     this.database = this.database.filter((record) => record.id !== id);
     return Promise.resolve();
   }
 
+  /**
+   * Validates a record entry to verify schema matching
+   * @param entry - The record data to sanitize
+   * @returns {boolean}
+   */
   sanitize(entry) {
 
     let valid = true;
--- src/models/mongo.js
+++ src/models/mongo.js
@@ -1,11 +1,24 @@
 'use strict';
 
+/** Class representing a generic mongo model.
+ *
+ *  Acknowledgements: Thanks to Vinicio Sanchez and Code Fellows for providing
+ *  this interface.
+ */
 class Model {
 
+  /**
+   * Model Constructor
+   * @param schema {object} - mongo schema
+   */
   constructor(schema) {
     this.schema = schema;
   }
 
+  /**
+   * JSON Schema
+   * @returns {*}
+   */
   jsonSchema() {
     console.log(typeof this.schema.jsonSchema);
     return typeof this.schema.jsonSchema === 'function'
@@ -13,11 +26,21 @@ class Model {
       : {};
   }
 
+  /**
+   * Retrieves one or more records
+   * @param _id {string} optional mongo record id
+   * @returns {*}
+   */
   get(_id) {
     let queryObject = _id ? { _id } : {};
     return this.schema.find(queryObject);
   }
 
+  /**
+   * Create a new record
+   * @param record {object} matches the format of the schema
+   * @returns {*}
+   */
   create(record) {
     console.log('r',record);
     let newRecord = new this.schema(record);
@@ -25,10 +48,21 @@ class Model {
     return newRecord.save();
   }
 
+  /**
+   * Replaces a record in the database
+   * @param _id {string} Mongo Record ID
+   * @param record {object} The record data to replace. ID is a required field
+   * @returns {*}
+   */
   update(_id, record) {
     return this.schema.findByIdAndUpdate(_id, record, { new: true });
   }
 
+  /**
+   * Deletes a recod in the model
+   * @param _id {string} Mongo Record ID
+   * @returns {*}
+   */
   delete(_id) {
     return this.schema.findByIdAndDelete(_id);
   }
