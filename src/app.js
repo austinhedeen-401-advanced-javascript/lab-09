00367db61c3053a73b7ae9fbc9b8bc2f18e348eb
Austin Hedeen <austin.hedeen@gmail.com>
2019-09-08 18:10:17 -0700

    Add Swagger config to main app

--- src/app.js
+++ src/app.js
@@ -16,10 +16,35 @@ const morgan = require('morgan');
 const errorHandler = require( `${cwd}/src/middleware/500.js`);
 const notFound = require( `${cwd}/src/middleware/404.js` );
 const v1Router = require( `${cwd}/src/api/v1.js` );
-const swagger = require(`${cwd}/src/api/swagger.js`);
 
 // Prepare the express app
 const app = express();
+
+const expressSwagger = require('express-swagger-generator')(app);
+
+let options = {
+  swaggerDefinition: {
+    info: {
+      description: 'API Server',
+      title: 'Swaggertastic Docs!',
+      version: '1.0.1',
+    },
+    host: 'localhost:3000',
+    basePath: '',
+    produces: [
+      'application/json',
+    ],
+    schemes: ['http'],
+    securityDefinitions: {
+      basicAuth: {
+        type: 'basic',
+      },
+    },
+  },
+  basedir: __dirname, //app absolute path
+  files: ['./api/v1.js'], //Path to the API handle folder
+};
+expressSwagger(options);
 
 // App Level MW
 app.use(cors());
