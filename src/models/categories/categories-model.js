715ac6baddb97888d042d5f4269b501bbd02fb4f
Austin Hedeen <austin.hedeen@gmail.com>
2019-09-08 17:54:45 -0700

    Document classes that extend base Model

--- src/models/categories/categories-model.js
+++ src/models/categories/categories-model.js
@@ -3,6 +3,10 @@
 const Model = require('../mongo.js');
 const schema = require('./categories-schema.js');
 
+/**
+ * Class representing a Category.
+ * @extends Model
+ */
 class Categories extends Model {
   constructor() { super(schema); }
 }
